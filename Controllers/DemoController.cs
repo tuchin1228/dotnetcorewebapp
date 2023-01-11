4b01453ba62af18327aadfbf4b9c34f388c900ec
tuchin1228 <jhinwei1228@gmail.com>
2023-01-11 16:41:51 +0800

    加入專案檔案。

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcorewebapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        IConfiguration _configuration;
+
+        public DemoController(IConfiguration inConfig) 
+        {
+            _configuration = inConfig;
+        } 
+
+        [HttpGet]
+        public IActionResult index()
+        {
+            return Ok(new
+            {
+                message="index"
+            });
+        }
+
+        [HttpGet("index2")]
+        public IActionResult index2()
+        {
+            return Ok(new
+            {
+                message = "new " + _configuration["env"].ToString()
+            });
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetcorewebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
